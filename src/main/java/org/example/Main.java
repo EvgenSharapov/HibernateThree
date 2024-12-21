efbfd929d2f82ae48982f5ea9992ec618473fe44
EvgenSharapov <dooplert@yandex.ru>
2024-12-22 00:11:42 +0300

    dao1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,67 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.javarush.dao.CityDAO;
+import com.javarush.dao.CountryDAO;
+import com.javarush.domain.City;
+import com.javarush.repository.RepositoryConnections;
+import com.mysql.cj.xdevapi.SessionFactory;
+import io.lettuce.core.RedisClient;
+import org.hibernate.Session;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static java.util.Objects.nonNull;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private final RedisClient redisClient;
+
+    private final ObjectMapper mapper;
+
+    public Main() {
+        redisClient = prepareRedisClient();
+        mapper = new ObjectMapper();
     }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        List<City> allCities = main.fetchData();
+        main.shutdown();;
+    }
+
+
+    private List<City> fetchData() {
+        try (Session session = RepositoryConnections.getInstance().getSessionFactory().getCurrentSession()) {
+            List<City> allCities = new ArrayList<>();
+            session.beginTransaction();
+            CityDAO cityDAO = RepositoryConnections.getInstance().getCityDAO();
+
+            int totalCount = cityDAO.getTotalCount();
+            int step = 500;
+            for (int i = 0; i < totalCount; i += step) {
+                allCities.addAll(cityDAO.getItems(i, step));
+            }
+            session.getTransaction().commit();
+            return allCities;
+        }
+    }
+
+    private void shutdown() {
+        if (nonNull(RepositoryConnections.getInstance().getSessionFactory())) {
+            RepositoryConnections.getInstance().getSessionFactory().close();
+        }
+        if (nonNull(redisClient)) {
+            redisClient.shutdown();
+        }
+    }
+
+    private RedisClient prepareRedisClient(){
+        return null;
+    }
+
+
+
 }
